3c6e02e9948124dfefd83867fdad3d94fea491ad
Julio Alexandre <julioalexandre499@gmail.com>
2023-11-29 08:19:33 -0300

    deployconfig

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react_online_store",
   test: {
     include: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
     globals: true,
